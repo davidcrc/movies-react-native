c014bce997e0f2eb950466a33b94ba97640d2ac0
david <david_drcr@hotmail.com>
2021-08-16 12:32:21 -0500

    first

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, SafeAreaView, Text } from 'react-native'
+
+const App = () => {
+  console.log("hola lola")
+  return (
+    <View>
+      <Text>hello world</Text>
+    </View>
+  )
+}
+
+export default App
